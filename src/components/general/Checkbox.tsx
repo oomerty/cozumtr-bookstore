bf2c7af772b77551278a8b7986582157a9f84e22
Ömer Mert ERYİĞİT <59279876+oomerty@users.noreply.github.com>
2025-07-02 16:32:41 +0300

    Checkbox color fix

--- src/components/general/Checkbox.tsx
+++ src/components/general/Checkbox.tsx
@@ -12,7 +12,7 @@ function Checkbox({ id, label, disabled, onClick }: CheckboxProps) {
         type="checkbox"
         id={id}
         name={id}
-        className="appearance-none w-4 h-4 border-3 border-indigo-600 rounded-xs bg-white checked:bg-indigo-800 transition duration-150 checked:rotate-180"
+        className="appearance-none w-4 h-4 border-3 border-indigo-600 rounded-xs bg-white checked:bg-indigo-600 transition duration-150 checked:rotate-180"
       />
       <label htmlFor={id} className="text-indigo-600 text-sm font-bold">
         {label}
